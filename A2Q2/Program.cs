c621e7f310ec32eb50d0b578312d3ae470865ae4
Julian Milligan <56824914+1904804@users.noreply.github.com>
2021-01-31 20:58:28 +0000

    1

--- A2Q2/Program.cs
+++ A2Q2/Program.cs
@@ -10,6 +10,25 @@ namespace A2Q2
     {
         static void Main(string[] args)
         {
+
+            double vectorValue;
+            double vectorNormalized;
+
+            Console.WriteLine("Please enter the value of the vector you wish to be normalized - ");
+            vectorValue = Convert.ToDouble(Console.ReadLine());
+
+            if ( vectorValue == 0)//to ajust is zero is entered
+            {
+                //function to calculate normlized value and value is ajusted to cope a zero entry
+                vectorValue = vectorValue + 1;
+                vectorNormalized = vectorValue / vectorValue;
+                Console.WriteLine("The normalized value of the vector you entered is - " + vectorNormalized);
+
+            }else
+            {
+                vectorNormalized = vectorValue / vectorValue;
+                Console.WriteLine("The normalized value of the vector you entered is - " + vectorNormalized);
+            }
         }
     }
 }
